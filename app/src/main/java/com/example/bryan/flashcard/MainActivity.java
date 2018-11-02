24681d7e8d349131a28b94f051b5f00f7ed44098
Bryan Caldera <bryanicaldera@gmail.com>
2018-11-02 07:59:51 -0700

    First upload

--- app/src/main/java/com/example/bryan/flashcard/MainActivity.java
+++ app/src/main/java/com/example/bryan/flashcard/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.bryan.flashcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.content.*;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
+
+        findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, add_question.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            String question = data.getExtras().getString("question");
+            String answer = data.getExtras().getString("answer");
+
+            ((TextView) findViewById(R.id.flashcard_question)).setText(question);
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
+
+            Log.d("data", question + answer);
+        }
     }
 }
---
+++ app/src/main/java/com/example/bryan/flashcard/add_question.java
@@ -0,0 +1,39 @@
+package com.example.bryan.flashcard;
+
+import android.content.Intent;
+import android.os.Debug;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.EditText;
+
+
+public class add_question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_question);
+
+        findViewById(R.id.imageButton2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.imageButton3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("question", ((EditText) findViewById(R.id.editText)).getText().toString());
+                data.putExtra("answer", ((EditText) findViewById(R.id.editText2)).getText().toString());
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish();
+
+                Log.d("myTag", "Save button ran");
+            }
+        });
+    }
+}
